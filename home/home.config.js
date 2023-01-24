16228c828882ed4002cfc584e5d77566d46b64ed
Tristen Spruill <Tspruil6@uncc.edu>
2023-01-24 17:43:50 -0500

    first commit

---
+++ home/home.config.js
@@ -0,0 +1,12 @@
+function HomeConfig($stateProvider) {
+  'ngInject';
+
+  $stateProvider.state('home', {
+    url: '',
+    controller: 'HomeCtrl',
+    controllerAs: '$ctrl',
+    templateUrl: 'home/home.html',
+  });
+}
+
+export default HomeConfig;
